4c8b8992ac7e98933fb91e1dd6fe68973188a641
Abhishek Anand <abhishek788@gmail.com>
2020-10-10 19:45:18 +0530

    sum

---
+++ LeetCode/Medium/Sum-of-Two-Integers.cs
@@ -0,0 +1,25 @@
+/*
+https://leetcode.com/problems/sum-of-two-integers/
+Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = -2, b = 3
+Output: 1
+*/
+
+public class Solution {
+    public int GetSum(int a, int b) {
+        int c; 
+      while(b !=0 ) {
+        c = (a&b);
+        a = a ^ b;
+        b = (c)<<1;
+      }
+      return a;
+    }
+}
