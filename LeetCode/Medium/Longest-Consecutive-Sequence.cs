f9e2f962800610412bcf4824bb52ab124f6d18bc
Abhishek Anand <abhishek788@gmail.com>
2021-06-02 14:18:45 +0530

    changes

---
+++ LeetCode/Medium/Longest-Consecutive-Sequence.cs
@@ -0,0 +1,46 @@
+/*
+https://leetcode.com/problems/longest-consecutive-sequence/
+Given an unsorted array of integers nums, return the length of the longest consecutive elements sequence.
+
+You must write an algorithm that runs in O(n) time.
+
+ 
+
+Example 1:
+
+Input: nums = [100,4,200,1,3,2]
+Output: 4
+Explanation: The longest consecutive elements sequence is [1, 2, 3, 4]. Therefore its length is 4.
+Example 2:
+
+Input: nums = [0,3,7,2,5,8,4,6,0,1]
+Output: 9
+ 
+
+Constraints:
+
+0 <= nums.length <= 105
+-109 <= nums[i] <= 109
+*/
+
+public class Solution {
+    public int LongestConsecutive(int[] nums) {
+        var set = new HashSet<int>(nums);
+        var result = 0;
+        foreach(var num in set)
+        {
+            if(!set.Contains(num - 1))
+            {
+                var next = num + 1;
+                while(set.Contains(next))
+                {
+                    next++;
+                }
+                
+                result = Math.Max(result, next - num);
+            }
+        }
+        
+        return result;
+    }
+}
